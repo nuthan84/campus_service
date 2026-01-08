65e7911c98dcc19e663bdb22de5dbfe474bfd39a
nuthan84 <nuthanuthejvalluri@gmail.com>
2026-01-08 09:04:05 +0530

    Auto-create admin

--- users/apps.py
+++ users/apps.py
@@ -1,5 +1,15 @@
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
 
 class UsersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    def ready(self):
+        User = get_user_model()
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@example.com",
+                password="Admin@123"
+            )
