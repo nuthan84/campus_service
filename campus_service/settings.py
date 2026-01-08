c52bc97dead5be1310f5b18e82f35c2ddd5e3889
nuthan84 <nuthanuthejvalluri@gmail.com>
2026-01-08 17:37:37 +0530

    Enable PyMySQL backend

--- campus_service/settings.py
+++ campus_service/settings.py
@@ -59,7 +59,8 @@ WSGI_APPLICATION = 'campus_service.wsgi.application'
 DATABASES = {
     "default": dj_database_url.config(
         default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require=False
     )
 }
 
