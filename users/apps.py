ae46a273bb07f1e9819ac2174b015ed61027c011
nuthan84 <nuthanuthejvalluri@gmail.com>
2026-01-08 17:59:48 +0530

    Set DEBUG handling

--- users/apps.py
+++ users/apps.py
@@ -1,8 +1,17 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
+import os
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
 
     def ready(self):
-        pass
+        User = get_user_model()
+        if os.environ.get("CREATE_ADMIN_USER", "False") == "True":
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    username="admin",
+                    email="admin@example.com",
+                    password="Admin@123"
+                )
